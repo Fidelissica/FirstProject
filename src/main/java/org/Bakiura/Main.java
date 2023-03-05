4d9e65135da3c3be197b08360cdd7fcd1228b595
Fidelissica <127040746+Fidelissica@users.noreply.github.com>
2023-03-05 17:37:05 +0100

    Added money attribute to Person class

--- src/main/java/org/Bakiura/Main.java
+++ src/main/java/org/Bakiura/Main.java
@@ -5,10 +5,10 @@ public class Main {
         System.out.println("That's my first application");
         System.out.println("Hello world!");
 
-        Person person1 = new Person("Adam", "P", 12, 1);
+        Person person1 = new Person("Adam", "P", 12, 1, 1000.0F);
         person1.showPerson();
 
-        Person person2 = new Person("Bartek", "P", 6, 1);
+        Person person2 = new Person("Bartek", "P", 6, 1, 350.0F);
         person2.showPerson();
     }
 }
--- src/main/java/org/Bakiura/Person.java
+++ src/main/java/org/Bakiura/Person.java
@@ -5,17 +5,20 @@ public class Person {
     private final String lastname;
     private final int age;
     private final int gender;
+    private final float money;
 
-    Person(String firstname, String lastname, int age, int gender) {
+    Person(String firstname, String lastname, int age, int gender, float money) {
         this.firstname = firstname;
         this.lastname = lastname;
         this.age = age;
         this.gender = gender;
+        this.money = money;
     }
 
     public void showPerson() {
         String gender = getGender();
-        System.out.println(String.format("Firstname: %s\nLastname: %s\nAge: %d\nGender: %s", this.firstname, this.lastname, this.age, gender));
+        System.out.println(String.format("Firstname: %s\nLastname: %s\nAge: %d\nGender: %s\nMoney: %.2f",
+                this.firstname, this.lastname, this.age, gender, this.money));
     }
 
     private String getGender() {
